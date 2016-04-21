48dc3d8d6a9ae9421650947d70c5cbb0ecebcd1a
Martin Zeller <mz.bremerhaven@gmail.com>
2016-04-21 12:25:31 +0200

    Add type-level modes to streams

--- src/Control/Monad/Eff/OpenComputers/Stream.purs
+++ src/Control/Monad/Eff/OpenComputers/Stream.purs
@@ -1,6 +1,12 @@
 module Control.Monad.Eff.OpenComputers.Stream
     ( Stream
     , Whence(..)
+    , Read
+    , Write
+    , Binary
+    , Seek
+    , Readable
+    , Writable
     , read
     , write
     , close
@@ -11,24 +17,32 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Either (Either(..))
 
-data Stream (e :: # !)
+foreign import data Stream :: # * -> # ! -> *
+
+type Readable r = Stream (read :: Read | r)
+type Writable r = Stream (write :: Write | r)
+
+data Read
+data Write
+data Binary
+data Seek
 
 data Whence = Set
             | Cur
             | End
 
-foreign import readImpl :: forall e a b. (a -> Either a b) -> (b -> Either a b) -> Stream e -> Int -> Eff e (Either String String)
-read :: forall e. Stream e -> Int -> Eff e (Either String String)
+foreign import readImpl :: forall r e a b. (a -> Either a b) -> (b -> Either a b) -> Readable r e -> Int -> Eff e (Either String String)
+read :: forall r e. Readable r e -> Int -> Eff e (Either String String)
 read = readImpl Left Right
 
-foreign import writeImpl :: forall e a b. (a -> Either a b) -> (b -> Either a b) -> Stream e -> String -> Eff e (Either String Unit)
-write :: forall e. Stream e -> String -> Eff e (Either String Unit)
+foreign import writeImpl :: forall r e a b. (a -> Either a b) -> (b -> Either a b) -> Writable r e -> String -> Eff e (Either String Unit)
+write :: forall r e. Writable r e -> String -> Eff e (Either String Unit)
 write = writeImpl Left Right
 
-foreign import close :: forall e. Stream e -> Eff e Unit
+foreign import close :: forall r e. Stream r e -> Eff e Unit
 
-foreign import seekImpl :: forall e a b. (a -> Either a b) -> (b -> Either a b) -> Stream e -> String -> Int -> Eff e (Either String Int)
-seek :: forall e. Stream e -> Whence -> Int -> Eff e (Either String Int)
+foreign import seekImpl :: forall r e a b. (a -> Either a b) -> (b -> Either a b) -> Stream (seek :: Seek | r) e -> String -> Int -> Eff e (Either String Int)
+seek :: forall r e. Stream (seek :: Seek | r) e -> Whence -> Int -> Eff e (Either String Int)
 seek stream = seekImpl Left Right stream <<< whenceToLua
 
 whenceToLua :: Whence -> String
